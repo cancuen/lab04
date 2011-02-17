a7c74014eb61d9bc7b8b5694178033405c77ca44
Cancuen <cancuen.net@gmail.com>
2011-02-17 14:39:28 -0600

    ejercicio 3 completo

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,7 @@
 class Order < ActiveRecord::Base
 	belongs_to :clients
 	has_many :products
+	
+	#createOrder(clientNit, productCode)
+	@order = Order.createOrder(:nit => client.nit, :code => @order.code)
 end
